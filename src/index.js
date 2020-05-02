c61d77e8695fe4ba5884801c0f2e1d9edd38782c
luzik000 <liza000@gmail.com>
2020-05-02 23:51:09 +0300

    added redux, and implemented userRedux and userActions to header and app components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
+import "./index.css";
 
-import App from './App';
+import store from "./redux/store";
+import App from "./App";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
