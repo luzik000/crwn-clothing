cf10cb5a542fe79f78911f829b479bb425f308c7
luzik000 <liza000@gmail.com>
2020-05-03 13:08:12 +0300

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/pages/shoppage/shoppage.component.js
+++ src/pages/shoppage/shoppage.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import SHOP_DATA from "./shop.date.js";
+import SHOP_DATA from "./shop.data.js";
 import "./shoppage.styles.scss";
 import CollectionPreview from "../../components/collection-preview/collection-preview.component.js";
 
